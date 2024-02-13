7ef8065c3e3c71ff98f4cc8551a1244ac1f435eb
mlh2020 <mauhampwork@gmail.com>
2024-02-13 04:08:16 -0800

    Added additional character details

--- src/Components/CharacterDetails.jsx
+++ src/Components/CharacterDetails.jsx
@@ -1,11 +1,14 @@
 
 function CharacterDetails({ character, onClose }) {
     return (
-        <div style={{ position: 'fixed', top: '20%', left: '30%', backgroundColor: 'white', padding: '20px', zIndex: 100 }}>
-        <h2>{character.name}</h2>
-        <p>Status: {character.status}</p>
-        <p>Species: {character.species}</p>
-        <p>Gender: {character.gender}</p>
+        <div style={{ position: 'fixed', top: '20%', left: '30%', backgroundColor: 'black', padding: '20px', zIndex: 100 }}>
+            <h2>{character.name}</h2>
+            <p>Status: {character.status}</p>
+            <p>Species: {character.species}</p>
+            <p>Gender: {character.gender}</p>
+            <p>Location: {character.location.name}</p>
+            <p>Origin: {character.origin.name}</p>
+            
         <img src={character.image} alt={character.name} />
         <button onClick={onClose}>Close</button>
         </div>
